d3316aca3d60cd7005b65c962b45c2828bc348cb
AMPLIFY-AWS <alex1232874@outlook.com>
2024-06-25 14:39:18 -0400

    inital commit

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>Chicken door status:</code> OPEN
         </p>
+
+        <p>Weather status: CLEAR AND SUNNY</p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
